d5be3d01eeacc57e8570a6e18398ec29120792cf
Reuben Li <mesolithic@gmail.com>
2018-07-21 11:38:40 +0900

    Update trade.py

--- src/trade.py
+++ src/trade.py
@@ -1,8 +1,5 @@
 %matplotlib tk
-import requests
 import time
-import hmac
-import hashlib
 import binance
 import pandas as pd
 from pprint import pprint
@@ -10,31 +7,67 @@ import matplotlib.pyplot as plt
 from quoine.client import Quoinex
 import json
 import pybitflyer
+import python_bitbankcc
+import os
+import yaml
+from zaifapi import *
+from multiprocessing import Process
 
-# insert auth credentials (api key and secret)
-auth = {}
+BTCJPY = 808865
+JPY_MIN = 1000
+BTC_MIN = 0.001
+BF_FEES = 0.0016
+EXCHANGES = ['bb', 'zf']
+MARGIN = 0.0001
+COLORS = ['blue', 'green', 'red', 'orange']
 
-# this is not valid, usually mininum is 0.001
-SIZE = 0.0001 
-BITFLYER_FEES = 0.0015
-WINDOW = 30
+## import credentials
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
-# use existing clients / api wrappers
-qn_client = Quoinex(auth['qn']['key'], auth['qn']['secret'])
+auth = cfg['auth']
+
+# deposit fees
+# zf = 486 jpy
+# bb = free
+# gm = free
+
+# withdrawal transfer fees
+# zf = 0.0005 BTC / 350 jpy
+# bb = 0.001 BTC / 540 jpy
+# gm = free
+
+## instantiate clients
+# qn_client = Quoinex(auth['qn']['key'], auth['qn']['secret'])
 bf_client = pybitflyer.API(api_key=auth['bf']['key'], api_secret=auth['bf']['secret'])
-binance.set(auth['bn']['key'], auth['bn']['secret'])
+# binance.set(auth['bn']['key'], auth['bn']['secret'])
+bb_client_pte = python_bitbankcc.private(auth['bb']['key'], auth['bb']['secret'])
+bb_client = python_bitbankcc.public()
+zf_pclient = ZaifTradeApi(auth['zf']['key'], auth['zf']['secret'])
+zf_client = ZaifPublicApi()
 
-def bf_trade(direction, size=SIZE):
-    """Trade with bitflyer client"""
-    bf_client.sendchildorder(
+def bf_trade(direction, size=0.001):
+    a = bf_client.sendchildorder(
         product_code="BTC_JPY",
         child_order_type="MARKET",
         side=direction,
         size=size
     )
+    print(a)
     
-def qn_trade(direction, size=SIZE):
-    """Trade with quoinex client"""
+def bf_price():
+    res = bf_client.ticker(product_code="BTC_JPY")
+    return res['best_ask'], res['best_bid']
+
+def bf_portfolio():
+    for i in bf_client.getbalance():
+        if i['currency_code'] == 'JPY':
+            bf_jpy = i['amount']
+        elif i ['currency_code'] == 'BTC':
+            bf_btc = i['amount']
+    return bf_jpy, bf_btc
+    
+def qn_trade(direction, size=0.001):
     if direction == 'BUY':
         qn_client.create_market_buy(
             product_id=5,
@@ -47,80 +80,165 @@ def qn_trade(direction, size=SIZE):
         )
     else:
         print('indicate BUY or SELL')
-
-def portfolio_value(data, funds):
-    """Check current portfolio value"""
-    for i in qclient.get_account_balances():
+        
+def qn_portfolio():
+    for i in qn_client.get_account_balances():
         if i['currency'] == 'JPY':
             qn_jpy = float(i['balance'])
         elif i ['currency'] == 'BTC':
             qn_btc = float(i['balance'])
-    for i in bf_client.getbalance():
-        if i['currency_code'] == 'JPY':
-            bf_jpy = i['amount']
-        elif i ['currency_code'] == 'BTC':
-            bf_btc = i['amount']
-    print('-----------------')
-    print('qn_jpy:', qn_jpy, 'bf_jpy:', bf_jpy, 'total:', qn_jpy+bf_jpy)
-    print('qn_btc:', qn_btc, 'bf_btc:', bf_btc, 'total:', qn_btc+bf_btc)
-    print('-----------------')
+    return qn_jpy, qn_btc
+        
+def bb_trade(direction, size=0.001):
+    bb_client_pte.order(
+        'btc_jpy',
+        '',
+        size,
+        direction.lower(),
+        'market'
+    )
     
-def trade_decision(data, funds):
-    """Decide whether to make a trade"""
-    # to-do: logic to purchase only if both sides have sufficient funding
-    if data['bitflyer_bid'] * (1 - BITFLYER_FEES) > data['quoinex_ask']:
-        print('buy @quoinex, sell @bitflyer')
-        #qn_trade('BUY')
-        #bf_trade('SELL')
-        portfolio_value(data, funds)
-    elif data['bitflyer_ask'] * (1 + BITFLYER_FEES) < data['quoinex_bid']:
-        print('buy @bitflyer, sell @quoinex')
-        #qn_trade('SELL')
-        #bf_trade('BUY')
-        portfolio_value(data, funds)
+def bb_price():
+    res = bb_client.get_ticker('btc_jpy')
+    return float(res['sell']), float(res['buy'])
+
+def bb_portfolio():
+    assets = bb_client_pte.get_asset()['assets']
+    for a in assets:
+        if a['asset'] == 'jpy':
+            bb_jpy = float(a['onhand_amount'])
+        elif a['asset'] == 'btc':
+            bb_btc = float(a['onhand_amount'])
+    return bb_jpy, bb_btc
+
+def zf_trade(direction, size=0.001):
+    ask, bid = zf_price()
+    if direction == 'BUY':
+        action = 'bid'
+        price = ask
     else:
-        pass
+        action = 'ask'
+        price = bid
+    res = zf_pclient.trade(
+        currency_pair='btc_jpy',
+        action=action,
+        amount=size,
+        price=int(price)
+    )
+    return res
 
-def huat_ah():
-    """the main()"""
-    d = []
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    print("start!")
+def zf_price():
+    res = zf_client.ticker('btc_jpy')
+    return res['ask'], res['bid']
 
-    # lame loop to be replaced by something cooler
-    for i in range(100):
-        js_bf = bf_client.ticker(product_code="BTC_JPY")
-        js_qn = qn_client.get_product(product_id=5)
-        # timestamp = str(round(time.time()))
-        data = {
-            #'time': timestamp,
-            'bitflyer_ask': js_bf['best_ask'],
-            'bitflyer_bid': js_bf['best_bid'],
-            'quoinex_ask': float(js_qn['market_ask']),
-            'quoinex_bid': float(js_qn['market_bid'])
-        }
-        trade_decision(data, funds)
+def zf_portfolio():
+    a = zf_pclient.get_info()
+    return a['funds']['jpy'], a['funds']['btc']
 
-        d.append(data)
+def portfolio_value():
+    table = {}
+    table['jpy'] = {}
+    table['btc'] = {}
+    status = {}
+    
+    for e in EXCHANGES:
+        status[e] = {'buy':True, 'sell':True}
+        jpy, btc = globals()[e + '_portfolio']()
+        if jpy < JPY_MIN:
+            status[e]['buy'] = False
+        elif btc < BTC_MIN:
+            status[e]['sell'] = False
+        table['jpy'][e] = jpy
+        table['btc'][e] = btc
         
-        # store only moving window to keep 
-        if i > WINDOW:
-            d.pop(0)
-        rows = -WINDOW
+    table['jpy']['_total'] = sum(table['jpy'].values())
+    table['btc']['_total'] = sum(table['btc'].values())
+    table['total_value'] = (table['btc']['_total'] * BTCJPY) + table['jpy']['_total']
+    return table, status
+    
+def trade_data(status):
+    action = ''
+    data = {}
+    min_ask = 2000000
+    max_bid = 1
+    
+    # best ask and bid exchanges
+    ask_e = ''
+    bid_e = ''
+    
+    for e in EXCHANGES:
+        ask, bid = globals()[e + '_price']()
         
-        df = pd.DataFrame(data=d)
+        if (max_bid - ask) / max_bid > MARGIN:
+            if status[e]['buy'] and status[bid_e]['sell']:
+                globals()[e + '_trade']('BUY')
+                globals()[bid_e + '_trade']('SELL')
+                print('opp! buy:' + e + ' sell: '+ bid_e + ' margin: '  + str(margin))
+                action = 'traded'
+            else:
+                action = 'no funds'
+        elif (bid - min_ask) / bid > MARGIN:
+            if status[ask_e]['buy'] and status[e]['sell']:
+                globals()[ask_e + '_trade']('BUY')
+                globals()[e + '_trade']('SELL')
+                print('opp! buy:' + ask_e + ' sell:'+ e + ' margin: '  + str(margin))
+                action = 'traded'
+            else:
+                action = 'no funds'
+        else:
+            action = 'no opportunity'
+            
+        if ask < min_ask:
+            min_ask = ask
+            ask_e = e
+        if bid > max_bid:
+            max_bid = bid
+            bid_e = e
 
-        # dynamically updating plot
-        ax.clear()
-        ax.plot(df.index[rows:], df['bitflyer_ask'][rows:], label='bf_ask')
-        ax.plot(df.index[rows:], df['bitflyer_bid'][rows:], label='bf_bid')
-        ax.plot(df.index[rows:], df['quoinex_ask'][rows:], label='qn_ask')
-        ax.plot(df.index[rows:], df['quoinex_bid'][rows:], label='qn_bid')
-        ax.legend()
+        data[e + '_ask'] = ask
+        data[e + '_bid'] = bid
 
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        time.sleep(1.5)
-        
-huat_ah()
+    return data, action
+
+d = []
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+print('-------------')
+print("START!")
+print("-------------")
+
+i = 0
+last_action = ''
+while True:
+    i += 1
+    table, status = portfolio_value()    
+    data, action = trade_data(status)
+    
+    if action == 'traded' or action != last_action:
+        print(action)
+        pprint(table)
+        pprint(status)
+        print("-------------")
+    
+    last_action = action
+
+    d.append(data)
+    if i > 30:
+        d.pop(0)
+    df = pd.DataFrame(data=d)
+    
+    rows = -30
+    # clear the list
+    ax.clear()
+    
+    e_color = 0
+    for e in EXCHANGES:
+        ax.plot(df.index[rows:], df[e+'_ask'][rows:], label=e+'_ask', color=COLORS[e_color])
+        ax.plot(df.index[rows:], df[e+'_bid'][rows:], label=e+'_bid', linestyle='dashed', color=COLORS[e_color])
+        e_color += 1
+    ax.legend()
+    
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    time.sleep(2)
