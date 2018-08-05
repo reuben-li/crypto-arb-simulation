f43d541117b7649d94ccfb9097fd4eaca4abdba8
Reuben Li <mesolithic@gmail.com>
2018-08-05 01:11:11 +0000

    refactored

--- src/margin.py
+++ src/margin.py
@@ -3,10 +3,10 @@ from quoine.client import Quoinex
 import time
 import json
 import yaml
-import threading
 from datetime import datetime, timedelta
 import logging
 import os
+import sys
 import tailer
 
 logging.basicConfig(filename='trade.log', level=logging.WARNING)
@@ -14,11 +14,12 @@ logging.basicConfig(filename='trade.log', level=logging.WARNING)
 # globals
 SIZE = 0.2
 MARGIN = 3000
-MIN_MARGIN = 1500
+PROFIT_THRESHOLD = 300 # in yen
 STABLE_VOL_FLOAT = 0.5
 LEVERAGE = 5
 QN_MC = 0.15
 ZF_MC = 0.35
+HISTORY = 49
 
 # import credentials
 with open('config.yml', 'r') as ymlfile:
@@ -62,15 +63,22 @@ def portfolio_value():
     qn_res = qn_client.get_trading_account(1464512)
     table['qn_balance'] = float(qn_res['equity'])
     table['qn_margin'] = float(qn_res['free_margin']) / table['qn_balance']
-    try:
-        key = 'deposit_jpy'
-        zf_used = next(iter(
-            zf_pclient.active_positions(type='margin').items()))[1][key]
-    except Exception:
-        zf_used = 0
-    table['zf_balance'] = zf_iclient.get_info2()['funds']['jpy'] + zf_used
+    table['qn_pnl'] = float(qn_res['pnl'])
+    zf_id, zf_pnl, zf_used = order_status_zf()
+    table['zf_balance'] = zf_iclient.get_info2()['funds']['jpy'] + \
+        zf_used + zf_pnl
     table['zf_margin'] = (table['zf_balance'] - zf_used) / table['zf_balance']
-    return table
+    table['zf_pnl'] = zf_pnl
+
+    if table['qn_margin'] == 1.0 and table['zf_margin'] == 1.0:
+        open_orders = False
+    else:
+        open_orders = True
+    # XOR
+    if bool(table['qn_margin'] == 1.0) ^ bool(table['zf_margin'] == 1.0):
+        print('!!! UNBALANCE TRADING !!!')
+        sys.quit(1)
+    return table, open_orders
 
 
 def open_qn(direction, price, size=SIZE, leverage=LEVERAGE):
@@ -86,12 +94,6 @@ def open_qn(direction, price, size=SIZE, leverage=LEVERAGE):
     )
 
 
-def close_qn():
-    res = qn_client.get_trades(funding_currency='JPY', status='open')
-    for lev in res['models']:
-        qn_client.close_trade(lev['id'])
-
-
 def open_zf(direction, price, size=SIZE, leverage=LEVERAGE):
     action = 'bid' if direction == 'BUY' else 'ask'
     zf_pclient.create_position(
@@ -102,6 +104,44 @@ def open_zf(direction, price, size=SIZE, leverage=LEVERAGE):
         type='margin',
         currency_pair='btc_jpy'
     )
+
+
+def order_status_qn():
+    res = qn_client.get_trades(funding_currency='JPY', status='open')
+    order_id = ''
+    pnl = 0
+    margin_used = 0
+    if res:
+        order_id = res['models'][0]['id']
+        pnl = res['models'][0]['open_pnl']
+        margin_used = res['models'][0]['margin_used']
+    return order_id, pnl, margin_used
+
+
+def order_status_zf(ask, bid):
+    order_id = ''
+    pnl = 0
+    margin_used = 0
+    res = zf_pclient.active_positions(type='margin')
+    if res:
+        ask, bid = zf_price()
+        for lev in res.items():
+            order_id = int(lev[0])
+            if lev[1]['action'] == 'ask':
+                diff = lev[1]['price_avg'] - ask
+            else:
+                diff = bid - lev[1]['price_avg']
+            pnl = diff * lev[1]['amount_done']
+            margin_used = lev[1]['deposit_jpy']
+            # taking only 1 item
+            break
+    return order_id, pnl, margin_used
+
+
+def close_qn():
+    res = qn_client.get_trades(funding_currency='JPY', status='open')
+    for lev in res['models']:
+        qn_client.close_trade(lev['id'])
 
 
 def close_zf():
@@ -120,28 +160,20 @@ def close_zf():
         )
 
 
-def clear_trade():
-    with open('current_trade.json', 'w') as outfile:
-        json.dump({'trade': ''}, outfile)
-
-
 def check_opp(trade_ok):
-    with open('current_trade.json', 'r') as infile:
-        ct = json.load(infile)
     qa, qb = qn_price()
     za, zb = zf_price()
-    if not ct['trade'] and trade_ok:
+    if trade_ok:
         if zb - qa > MARGIN:
             try:
                 open_zf('SELL', zb)
                 open_qn('BUY', '')
+                print('traded')
                 logging.warning(
                     str(datetime.now() + timedelta(hours=9)) +
                     ' buy ' + 'qn' + ':' +
                     str(qa) + ' sell ' + 'zf' + ':' + str(zb)
                 )
-                with open('current_trade.json', 'w') as outfile:
-                    json.dump({'trade': 'bqsz'}, outfile)
             except Exception:
                 print('trade failed')
                 pass
@@ -149,30 +181,22 @@ def check_opp(trade_ok):
             try:
                 open_zf('BUY', za)
                 open_qn('SELL', '')
+                print('traded')
                 logging.warning(
                     str(datetime.now() + timedelta(hours=9)) +
                     ' buy ' + 'zf' + ':' +
                     str(za) + ' sell ' + 'qn' + ':' + str(qb)
                 )
-                with open('current_trade.json', 'w') as outfile:
-                    json.dump({'trade': 'bzsq'}, outfile)
             except Exception:
                 print('trade failed')
                 pass
     else:
-        if (ct['trade'] == 'bqsz') and (za - qb < MIN_MARGIN):
+        qn_id, qn_pnl, qn_used = order_status_qn()
+        zf_id, zf_pnl, zf_used = order_status_zf()
+        if qn_pnl + zf_pnl > PROFIT_THRESHOLD:
             try:
                 close_zf()
                 close_qn()
-                clear_trade()
-            except Exception:
-                print('close failed')
-                pass
-        if (ct['trade'] == 'bzsq') and (qa - zb < MIN_MARGIN):
-            try:
-                close_zf()
-                close_qn()
-                clear_trade()
             except Exception:
                 print('close failed')
                 pass
@@ -181,39 +205,28 @@ def check_opp(trade_ok):
     data['zf_ask'] = za
     data['qn_bid'] = qb
     data['qn_ask'] = qa
-
-    d = sorted(data.items(), key=lambda kv: kv[1], reverse=True)
-    print('-------')
-    print('PRICES')
-    print('-------')
-    oldv = ''
-    for k, v in d:
-        if oldv:
-            print(k, v, '(' + str(v-oldv) + ')')
-        else:
-            print(k, v)
-        oldv = v
-    print('')
-    print('zf_ask - qn_bid:', za - qb)
-    return
+    return data
 
 
 def main():
-    i = 0
-    with open('table.json') as data_file:
-        old_table = json.load(data_file)
+    max_zbqa = 0
+    min_zaqb = 9999999
 
     while True:
         os.system('clear')
-        i += 1
-        table = portfolio_value()
-        if table['qn_margin'] > QN_MC and table['zf_margin'] > ZF_MC:
-            check_opp(True)
+        table, open_orders = portfolio_value()
+        if open_orders:
+            data = check_opp(False)
         else:
-            check_opp(False)
+            data = check_opp(True)
 
-        qn_pnl = table['qn_balance'] - old_table['qn_balance']
-        zf_pnl = table['zf_balance'] - old_table['zf_balance']
+        current_zbqa = data['zf_bid'] - data['qn_ask']
+        current_zaqb = data['zf_ask'] - data['qn_bid']
+        max_zbqa = max(max_zbqa, current_zbqa)
+        min_zaqb = max(min_zaqb, current_zaqb)
+
+        qn_pnl = table['qn_pnl']
+        zf_pnl = table['zf_pnl']
         print('-------')
         print('BALANCE')
         print('-------')
@@ -229,6 +242,24 @@ def main():
         print('free qn margin:', round(table['qn_margin'] * 100, 2), '%')
         print('free zf margin:', round(table['zf_margin'] * 100, 2), '%')
 
+        d = sorted(data.items(), key=lambda kv: kv[1], reverse=True)
+        print('-------')
+        print('PRICES')
+        print('-------')
+        oldv = ''
+        for k, v in d:
+            if oldv:
+                print(k, v, '(' + str(round(v-oldv, 2)) + ')')
+            else:
+                print(k, v)
+            oldv = v
+        print('')
+        print('cur zbqa:', round(current_zbqa, 2))
+        print('cur zaqb:', round(current_zaqb, 2))
+
+        print('max zbqa:', round(max_zbqa, 2))
+        print('min zaqb:', round(min_zaqb, 2))
+
         print('-------')
         print('RECENT TRADES')
         print('-------')
@@ -238,7 +269,7 @@ def main():
         with open('table.json', 'w') as outfile:
             json.dump(table, outfile)
 
-        time.sleep(4)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
